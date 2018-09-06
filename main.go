7ceabc2245d14221534860f8853c72ed11496962
Mvmo <entchenmomoyt@gmail.com>
2018-09-06 05:31:12 +0200

    simple echo test

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "github.com/labstack/echo"
+
+func main() {
+	srv := echo.New()
+
+	apiGroup := srv.Group("/api")
+	apiV1Group := apiGroup.Group("/v1")
+	apiV1Group.GET("test", func(context echo.Context) error {
+		return context.JSON(200, "Ok")
+	})
+
+	srv.Start(":8149")
+}
