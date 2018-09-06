7e0281df2d73dd13b9618ffabe3cc98fda02e3d5
Mvmo <entchenmomoyt@gmail.com>
2018-09-06 06:20:30 +0200

    create data file if not exist

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"os"
+	"fmt"
+)
+
+var (
+	databaseFile = "data.json"
+	boardMap = make(map[string]Board)
+)
+
+func init() {
+	if _, err := os.Stat(databaseFile); err != nil {
+		fmt.Println(databaseFile + " doesn't exist - Trying to create ->", err)
+		if file, err := os.Create(databaseFile); err != nil {
+			fmt.Println("Error while creating " + databaseFile + " file -> ", err)
+		} else {
+			fmt.Println("File successfully created. Trying to write content...")
+			if _, err := file.WriteString("{}"); err != nil {
+				fmt.Println("Error while trying to write into file -> ", err)
+			} else {
+				fmt.Println("File successfully initialized with default json string '{}'")
+			}
+		}
+	}
+}
+
 func main() {
 	/*srv := echo.New()
 
