64b3e36eb8efe449630a0a930eb4ebcb49b0eee7
Mvmo <entchenmomoyt@gmail.com>
2018-09-08 00:11:29 +0200

    nice print

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ import (
 
 var (
 	databaseFilename = "data.json"
-	databaseFile *os.File
-	boardMap = make(map[string]Board)
+	databaseFile     *os.File
+	boardMap         = make(map[string]Board)
 )
 
 func init() {
 	if _, err := os.Stat(databaseFilename); err != nil {
-		fmt.Println(databaseFilename + " doesn't exist - Trying to create ->", err)
+		fmt.Println(databaseFilename+" doesn't exist - Trying to create ->", err)
 		if file, err := os.Create(databaseFilename); err != nil {
-			fmt.Println("Error while creating " + databaseFilename + " file -> ", err)
+			fmt.Println("Error while creating "+databaseFilename+" file -> ", err)
 		} else {
 			fmt.Println("File successfully created. Trying to write content...")
 			databaseFile = file
@@ -37,7 +37,6 @@ func main() {
 	})
 
 	srv.Start(":8149")*/
-
 
 	/*board := Board{Password: "foo", UniqueId: "entendorf", Tasks: []Task{
 		{
@@ -62,7 +61,14 @@ func main() {
 	b, _ := ioutil.ReadFile(databaseFilename)
 	json.Unmarshal(b, &boardMap)
 
-	fmt.Println(boardMap)
+	//fmt.Println(boardMap)
+	for k, v := range boardMap {
+		fmt.Println(k)
+		for _, t := range v.Tasks {
+			fmt.Println(" -> " + t.Title)
+			fmt.Println("  - " + t.Description)
+		}
+	}
 
 	/*c, _ := ioutil.ReadFile(databaseFilename)
 	var j = make(map[string]interface{})
